5e1528aed8ce1b1d28e5572f18c07116148c1105
Sangco, Jerrold <98925358+Sangco428@users.noreply.github.com>
2024-02-22 15:04:33 +0800

    Update Lab3.3.py

--- LabAct4/Example 3/Lab3.3.py
+++ LabAct4/Example 3/Lab3.3.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 from matplotlib import pyplot as plt
-import numpy as np
+from matplotlib.animation import FuncAnimation
 
 image = cv.imread('images\\image1.jpg')
 cvrimg = cv.cvtColor(image, cv.COLOR_BGR2RGB) # This converts the color from RGB to BGR
@@ -9,7 +9,7 @@ x_axis = 135
 y_axis = 25
 ave = cv.blur(cvrimg,(2,2)) 
 
-laplacian = cv.Laplacian(ave, 6, cv.CV_64F)
+laplacian = cv.Laplacian(ave, 5, cv.CV_16F)
 filtered_image = cv.convertScaleAbs(laplacian)
 
 
@@ -19,13 +19,13 @@ plt.figure(figsize=(20,20))
 # show the first image
 plt.subplot(121)
 plt.imshow(cvrimg, cmap='gray')
-plt.title('original')
+plt.title('Original Image')
 plt.axis("off")
 
 # show the second image
 plt.subplot(122)
 plt.imshow(filtered_image, cmap='gray')
-plt.title('edged')
+plt.title('Edged Image')
 plt.axis("off")
 
 plt.show()
